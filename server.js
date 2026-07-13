2995cb2e31b96a691811ba4218fcdd80cd8df508
unknown <maheenfarhat11@gmail.com>
2026-07-14 00:25:14 +0500

    Initial backend server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Server is running' });
+});
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
